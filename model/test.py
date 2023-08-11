f24c4db75e3e855037c8067b38304a8de62af8b1
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2023-08-12 02:07:20 +0300

    print model summary

--- model/test.py
+++ model/test.py
@@ -1,5 +1,9 @@
 from unet import UNet
+from hyperparameters import get_hparams
+
 import torch
+from torchsummary import summary
+
 
 def test():
     # batch_size=3, in_channels=1, 160x160
@@ -8,6 +12,12 @@ def test():
     model = UNet(in_channels=1, out_channels=1)
     preds = model(x)
 
+    hyperparameters = get_hparams()
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+
+    summary(model.cuda(), (1, 160, 160))
 
     print(f"input shape: {x.shape}, prediction shape: {preds.shape}")
     assert preds.shape == x.shape, "Shapes are not identical for input and prediction"
