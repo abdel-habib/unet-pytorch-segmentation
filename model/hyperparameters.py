d6159ad424f3d8b28065e01ed67033aceb125b75
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2023-08-16 18:53:47 +0300

    Update hyperparameters.py

--- model/hyperparameters.py
+++ model/hyperparameters.py
@@ -4,9 +4,9 @@ import os
 def get_hparams():
     params = {
         "learning_rate": 1e-4,
-        "n_epochs": 50,
+        "n_epochs": 30,
         "batch_size": 2,
-        "size": (512, 512),
+        "size": (512,512),
         "checkpoint_path": os.path.join(os.getcwd(), "checkpoints/checkpoint.pth"),
         "results_path": os.path.join(os.getcwd(), "results/")
         }
